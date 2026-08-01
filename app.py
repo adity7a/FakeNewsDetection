3754797165b84d8cba17596f0b54ac522bd99bbb
adity7a <adityakumarpro302@gmail.com>
2026-08-02 01:03:38 +0530

    feat: add Dockerfile, requirements, include model files

--- app.py
+++ app.py
@@ -30,11 +30,12 @@ except Exception as e:
 #  DB CONNECT 
 try:
     db = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT", 26175)),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
     cursor = db.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS history (
